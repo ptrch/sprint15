897e0a440e24b74d500ecdb505f86c10e8e9cb65
Alexander Petrichenko <ptrch@ya.ru>
2020-09-11 16:18:04 +0300

    cancel commit

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const urlValid = require('validator').isURL;
 const emailValid = require('validator').isEmail;
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -36,7 +37,25 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'это поле является обязательным'],
     minlength: [8, 'мин. количество символов - 8'],
+    select: false,
   },
 });
 
+// eslint-disable-next-line func-names
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
 module.exports = mongoose.model('user', userSchema);
