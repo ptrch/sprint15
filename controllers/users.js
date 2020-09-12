f6ca365bdd33296a91a9ea5850119a6ede3fe053
Alexander Petrichenko <ptrch@ya.ru>
2020-09-12 12:42:51 +0300

    fix eslint err

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
-const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
